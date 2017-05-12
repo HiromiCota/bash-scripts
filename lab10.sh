ca28804f3dd9db880a53a8b8cfb0a80ae7c999c5
Hiromi Cota <hiromi.cota@gmail.com>
2017-05-11 18:41:40 -0700

    modified:   lab10.sh

--- lab10.sh
+++ lab10.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 
-TEMP=/root/filelist.txt
-why_en="N"
-COUNTER=0
-if [ -e "$TEMP" ]; then
-	echo "$TEMP already exists. This script will overwrite the file."
-	echo "Is this OK? (Y/N)"
-	read why_en
-	why_en=${why_en^^}
-	if [ "$why_en" != "Y" ]; then
-		echo "Exiting without changing "$TEMP"."
-		exit #Does this trigger cleanup?
-	fi
-fi
+FCOUNTER=0
+DCOUNTER=0
+TEMP=$(mktemp /tmp/filelist.XXXXXXXXXXX)
+quit(){
+	exit
+}
 cleanup(){
 	rm $TEMP
 }
-
-ls -LRa / > $TEMP
+trap quit SIGINT SIGTERM
+trap cleanup EXIT
+ls -Ra1 / > $TEMP
 
 while IFS= read -r VAR
 do
-	counter=${counter+1}
-	if [ "$VAR" ]; then #This should test to see if it's a file
-		echo "$VAR" | sed #Replace lead with File : 
-	elif [ "$VAR" ]; then #This should test to see if it's a dir
-		echo "$VAR" | sed #Replace lead with Directory :
-	#Ignore everything else.
+	if [[ $VAR == "./"* || $VAR == "/"* ]]; then #Directory test
+		((DCOUNTER++))
+		echo "Directory $DCOUNTER $VAR"
+	elif [[ $VAR == "." || $VAR == ".." || $VAR == "" || $VAR == ":" ]] ; then
+		: #Ignore header, blank lines, and pseudodirectories (. ..)
+	else #Looks like a file
+		((FCOUNTER++))
+		echo "File "$FCOUNTER" "$VAR
 	fi
-done < "TEMP"
+done < "$TEMP"
 
-trap cleanup EXIT
