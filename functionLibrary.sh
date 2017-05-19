44835e618549d5c7b1a99ef8e5f5cedd6ffb69e1
Hiromi Cota <hiromi.cota@gmail.com>
2017-05-18 19:03:27 -0700

    modified:   functionLibrary.sh 	new file:   lab11.sh

--- functionLibrary.sh
+++ functionLibrary.sh
@@ -1,18 +1,38 @@
-syntax() {
-if [[ "$2" == "-h" ]]; then
+getHelp() {
+echo $0" has the following syntax:"
+echo $0" -options <source directory> <target directory>"
+echo "<source directory> defaults to current directory."
+echo "<target directory> defaults to current user's home."
+echo "Valid options:"
+echo "-h Help (this message)"
+echo "-f Force move (move files to new directories, instead of copying)"
+echo "-s Specify source AND target directory"
+exit
+}
+
+getOpts() {
+RETURN=0
+while [ "$1" != "" ]; do
 	case $1 in
-		getYYMMDD|getMMDD|getYY|getMM|getDD)
-			echo $1 " usage: "$1" file_name"
+		-f )	RETURN=$((RETURN+1))
+			shift
 			;;
-		newDir)	
-			echo $1 " usage: "$1" directory_name"
+		-h )	getHelp
+			shift
+			RETURN=0				
 			;;
-	esac
-fi
+		-s )	RETURN=$((RETURN+2))
+			shift
+			;;
+		* )	
+			exit
+			;;
+	esac			
+done
+return RETURN
 }
 
 getYYMMDD() {
-syntax $0 $1
 local VAR=$(stat $1)
 local VAR=${VAR#*Modify: }
 local VAR=${VAR%% *}
@@ -20,38 +40,34 @@ echo $VAR
 }
 
 getMMDD() {
-syntax $0 $1
 local VAR=$(getYYMMDD $1)
 local VAR=${VAR#*-}
 echo $VAR
 }
 
 getYY() {
-syntax $0 $1
 local VAR=$(getYYMMDD $1)
 local VAR=${VAR%%-*}
 echo $VAR
 }
 
 getMM() {
-syntax $0 $1
 local VAR=$(getMMDD $1)
 local VAR=${VAR%-*}
 echo $VAR
 }
 
 getDD() {
-syntax $0 $1
 local VAR=$(getMMDD $1)
 local VAR=${VAR#-*}
 echo $VAR
 }
 
-newDir() {
-syntax $0 $1
-mkdir ~/$1
+quit() {
+exit
 }
 
-move() {
-syntax $0 $1
-if 
+cleanup() {
+:
+}
+
---
+++ lab11.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+source functionLibrary.sh
+
+trap quit SIGINT SIGTERM
+trap cleanup EXIT
+
+SOURCE=.
+TARGET=~
+TWO=2
+ONE=1
+ZERO=0
+OPTIONS=0
+OPTIONS=$(getOpts "$1" )
+if [[ "$OPTIONS" -ne 0 ]]; then
+	if [ "$OPTIONS" -gt "$TWO" ]; then
+		if [ -d $2 ]; then #Don't let users give garbage paths
+			SOURCE=$2
+		fi
+		if [ -d $3 ]; then
+			TARGET=$3
+		fi
+	fi
+	if [[ $((OPTIONS % 2 )) -eq "$ONE" ]]; then
+		FORCE=1
+	fi
+else
+	exit
+fi
+for f in $SOURCE; do
+	YEAR=$(getYY $f)
+	MONTH=$(getMM $f)
+	DAY=$(getDD $f)
+	TARGET="$TARGET/$YEAR" 
+	if [ ! -d $TARGET ]; then
+		mkdir $TARGET
+	fi
+	TARGET="$TARGET/$MONTH"
+	if [ ! -d $TARGET ]; then
+		mkdir $TARGET
+	fi
+	TARGET="$TARGET/$DAY"
+	if [ ! -d $TARGET ]; then
+		mkdir $TARGET
+	fi
+	if [ $FORCE -eq 1 ]; then
+		mv $f $TARGET
+	else
+		cp $f $TARGET
+	fi
+done
+	
