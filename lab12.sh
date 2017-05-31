c0eb4bd12e4047096c83855816b0baa1e537d6c1
Hiromi Cota <hiromi.cota@gmail.com>
2017-05-30 19:01:58 -0700

    new file:   lab12.sh

---
+++ lab12.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+LINE=""
+VAR=""
+OLDIFS=$IFS
+IFS=$'\n'
+quit(){
+exit
+}
+cleanup(){
+IFS=$OLDIFS
+}
+grab(){
+VAR=${LINE%%,*}
+}
+bump(){
+LINE=${LINE#*,}
+}
+trap quit SIGINT SIGTERM
+trap cleanup EXIT
+
+COUNTER=0
+declare -a FNAME
+declare -a LNAME
+declare -a COMP
+declare -a STREET
+declare -a CITY
+declare -a COUNTY
+declare -a STATE
+declare -a ZIP
+declare -a PHONE
+declare -a FAX
+declare -a EMAIL
+declare -a WEB
+
+while IFS= read -r LINE
+do
+	LINE=$(echo "$LINE" | sed 's/"//g')
+	grab; FNAME["$COUNTER"]="$VAR"; bump
+	grab; LNAME[$COUNTER]=$VAR; bump
+	grab; COMP[$COUNTER]=$VAR; bump
+	grab; STREET[$COUNTER]=$VAR; bump
+	grab; CITY[$COUNTER]=$VAR; bump
+	grab; COUNTY[$COUNTER]=$VAR; bump
+	grab; STATE[$COUNTER]=$VAR; bump
+	grab; ZIP[$COUNTER]=$VAR; bump
+	grab; PHONE[$COUNTER]=$VAR; bump
+	grab; FAX[$COUNTER]=$VAR; bump
+	grab; EMAIL[$COUNTER]=$VAR; bump
+	grab; WEB[$COUNTER]=$VAR; bump
+	((COUNTER++))
+done < "$1"	
+
+for (( i=0; i<COUNTER; i++ )) ; do
+	echo -n "\""${FNAME[$i]}"\",\""${LNAME[$i]}"\",\""${COMP[$i]}"\","
+	echo -n "\""${STREET[$i]}"\",\""${CITY[$i]}"\",\""${COUNTY[$i]}"\","
+	echo -n "\""${STATE[$i]}"\",\""${ZIP[$i]}"\",\""${PHONE[$i]}"\","
+	echo -n "\""${FAX[$i]}"\",\""${EMAIL[$i]}"\",\""${WEB[$i]}"\""
+	echo
+done
+IFS=$OLDIFS
+	
