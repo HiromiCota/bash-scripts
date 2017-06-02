512403c843c1c6f38800efc97a1d936884f84201
Hiromi Cota <hiromi.cota@gmail.com>
2017-06-01 19:35:43 -0700

    modified:   lab13.sh

--- lab13.sh
+++ lab13.sh
@@ -7,25 +7,33 @@ cleanup(){
 :
 }
 check_ip(){
-
+egrep '^((25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$' <<< "$1"
+local VAR=$?
+return "$VAR"
 }
 check_email(){
-
+egrep -q '^([a-zA-Z0-9\+!#\$%&\?\{|\}\*=]+)([a-ZA-Z0-9\+!#\$%&\?\{|\}\*=\.]*)@([a-ZA-Z0-9\+!#\$%&\?\{|\}\*=]*)\.([a-ZA-Z0-9\+!#\$%&\?\{|\}\*=\.]+)([a-zA-Z0-9\+!#\$%&\?\{|/}\*=])$' <<< "$1"
+local VAR=$?
+return "$VAR";
 }
 check_pn(){
-
+egrep -q '^([0-9]+)([-]?)([\(]?)([-0-9\)]*)([0-9])$' <<< "$1"
+local VAR=$?
+return "$VAR"
 }
 check_ccn(){
 
+local VAR=$?
+return "$VAR"
 }
-if [[ check_ip $1 ]]; then
-
-elif [[ check_email $1 ]]; then
-
-elif [[ check_pn $1 ]]; then
-
-elif [[ check_ccn $1 ]]; then
-
+if check_ip $1; then
+	echo "$1 is an IPv4 IP address."
+elif check_email $1; then
+	echo "$1 is (probably) an email address."
+elif check_pn $1; then
+	echo "$1 is a phone number."
+elif check_ccn $1; then
+	echo "$1 is a credit card number."
 else
 	echo "I don't know what that is."
 
