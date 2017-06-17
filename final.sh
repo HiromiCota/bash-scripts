d452f8c2b12ecfa47a380702b526584badb6cf7a
Hiromi Cota <hiromi.cota@gmail.com>
2017-06-16 20:38:00 -0700

    modified:   final.sh

--- final.sh
+++ final.sh
@@ -32,7 +32,7 @@ for f in $SOURCE; do
 	if [[ "$DEBUG" -eq 1 ]]; then
 		read hitAnyKey
 	fi
-	EXTENSION=${$1##*.}
+	EXTENSION=${1##*.}
 	EXTENSION=$(echo ${EXTENSION,,} )
 	HASH=$(tail -c1000 "$f" | md5sum)
 	grep -q "$HASH" "$HASHES"
