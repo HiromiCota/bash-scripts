3153b9fedde7b4ef4452a3cde2b1841f74671862
HiromiCota <HiromiCota@users.noreply.github.com>
2017-06-16 20:01:42 -0700

    Further along

--- library.sh
+++ library.sh
@@ -1,6 +1,10 @@
 quit() {
 exit
 }
+int() {
+logger "Program terminated by user"
+quit
+}
 cleanup() {
 if [ ! -d "$TEMP" ]; then
 	rm -rf "$TEMP"
@@ -23,10 +27,13 @@ export MODEL
 export HASH
 export HASHES=$(touch "$TEMP"/hashes)
 export LOG=$(touch "$TEMP"/log)
+export ERRLOG=$(touch "$TEMP"/final_error.log)
 export FAIL=0
 export TIME
 export DATE
 export PWD
+export EXTENSION
+export NAME
 }
 sizeCheck() {
 local FREE=$(df -k --output=avail "$1" | tail -n1)
@@ -36,6 +43,7 @@ if [[ "$FREE" -lt "$NEEDED" ]]; then
 	echo "Not enough space available!"
 	echo "$FREE""KB free."
 	echo "$NEEDED""KB needed."
+	"Insufficient space for temporary files. Halting." >> ERRLOG
 	exit
 else
 	return 0
@@ -47,13 +55,16 @@ exiv2 | grep "command not found"
 if [[ $? -eq 0 ]]; then
 	FAIL=1
 	echo "This script requirse exiv2 binary."
+	"Missing dependency: exiv2" >> ERRLOG
 fi
 md5sum final.sh | grep "command not found"
 if [[ $? -eq 0 ]]; then
 	FAIL=1
 	echo "This script requires md5sum binary."
+	"Missing dependency: md5sum" >> ERRLOG
 fi
 if [[ FAIL -eq 1 ]];then
+	"Exiting unsuccessfully." >> ERRLOG
 	exit
 fi
 }
@@ -95,7 +106,7 @@ set "$(exiv2 -g Exif.Image.DateTime -Pv "$1")"
 		MINUTE="$5"
 		SECOND="$6"
 		MAKE=$(exiv2 -g Exif.Image.Make -Pv "$1")
-		MODEL=$(exiv2 -g Exif.Image.Model -Pv "$1")
+		MODEL=$(exiv2 -g Exif.Image.Model -Pv "$1")		
  	else
 		setDATE "$1"
 		setTIME "$1"
@@ -108,6 +119,7 @@ set "$(exiv2 -g Exif.Image.DateTime -Pv "$1")"
 		MAKE="Unknown_Manufacturer"
 		MODEL="Unknown_Camera"
 	fi
+EXTENSION=${$1##*.}
 }
 logDirectory() {
 echo -e "\tDirectory : ""$1" >> "$LOG"
@@ -117,9 +129,9 @@ echo "File : ""$1"" ""$DATE"" ""$TIME"" ""$MAKE"" ""$MODEL" >> "$LOG"
 }
 logCopy() {
 if [[ "$FORCE" == "0" ]]; then
-	echo " copied to ""$1" >> "$LOG"
+	echo "$1"" copied to ""$2" >> "$LOG"
 else
-	echo " moved to ""$1" >> "$LOG"
+	echo "$1"" moved to ""$2" >> "$LOG"
 fi
 }
 copyFile() {
@@ -135,6 +147,14 @@ TARGET="$TARGET/$DAY"
 if [ ! -d "$TARGET" ]; then
 	mkdir "$TARGET"
 fi
-cp "$1" "$TARGET"/"$YEAR"-"$MONTH"-"$DAY"_"$HOUR"-"$MINUTE"-"$SECOND"_"$MAKE"_"$MODEL"
-logCopy "$TARGET"/"$YEAR"-"$MONTH"-"$DAY"_"$HOUR"-"$MINUTE"-"$SECOND"_"$MAKE"_"$MODEL"
+NAME=""$YEAR"-"$MONTH"-"$DAY"_"$HOUR"-"$MINUTE"-"$SECOND"_"$MAKE"_"$MODEL""
+local counter=1
+while [ -e "$TARGET"/"$NAME" ];do
+	NAME="$NAME""$counter"
+	(($counter++))
+done
+EXTENSION=${echo "$counter,,"}
+NAME+=".""$EXTENSION"
+cp "$1" "$TARGET"/"$NAME"
+logCopy "$1" "$TARGET"/"$NAME"
 }
