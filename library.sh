2f80d0f8b2ac3c9b242de57703d788c33aa58f0f
HiromiCota <HiromiCota@users.noreply.github.com>
2017-06-16 17:52:11 -0700

    Bug fixes

--- library.sh
+++ library.sh
@@ -2,7 +2,9 @@ quit() {
 exit
 }
 cleanup() {
-:
+if [ ! -d "$TEMP" ]; then
+	rm -rf "$TEMP"
+fi
 }
 setup() {
 export FORCE=0
@@ -27,13 +29,13 @@ export DATE
 export PWD
 }
 sizeCheck() {
-local FREE=`df -k --output=avail "$1" | tail -n1`
-local USED=`du -sb "$1" | cut -f1`
+local FREE=$(df -k --output=avail "$1" | tail -n1)
+local USED=$(du -sb "$1" | cut -f1)
 local NEEDED=$((USED*2))
 if [[ "$FREE" -lt "$NEEDED" ]]; then
 	echo "Not enough space available!"
-	echo ""$FREE"KB free."
-	echo ""$NEEDED"KB needed."
+	echo "$FREE""KB free."
+	echo "$NEEDED""KB needed."
 	exit
 else
 	return 0
@@ -56,23 +58,23 @@ if [[ FAIL -eq 1 ]];then
 fi
 }
 sanityCheck() {
-sizeCheck $1
+sizeCheck "$1"
 utilCheck
 }
 setDATE() {
-DATE=$(stat $1 | grep Modify | cut -d' ' -f2)
+DATE=$(stat "$1" | grep Modify | cut -d' ' -f2)
 }
 setYEAR() {
-YEAR=$(echo $DATE | cut -d'-' -f1)
+YEAR=$(echo "$DATE" | cut -d'-' -f1)
 }
 setMONTH() {
-MONTH=$(echo $DATE | cut -d'-' -f2)
+MONTH=$(echo "$DATE" | cut -d'-' -f2)
 }
 setDAY() {
-MONTH=$(echo $DATE | cut -d'-' -f3)
+MONTH=$(echo "$DATE" | cut -d'-' -f3)
 }
 setTIME() {
-TIME=$(stat $1 | grep Modify "$VAR" | cut -d' ' -f3)
+TIME=$(stat "$1" | grep Modify | cut -d' ' -f3)
 }
 setHOUR() {
 HOUR=$(echo "$TIME" | cut -c1-2)
@@ -84,7 +86,7 @@ setSECOND() {
 SECOND=$(echo "$TIME" | cut -c7-8)
 }
 setALL() {
-set $(exiv2 -g Exif.Image.DateTime -Pv "$1")
+set "$(exiv2 -g Exif.Image.DateTime -Pv "$1")"
 	if [[ $? -eq 0 ]]; then
 		YEAR="$1"
 		MONTH="$2"
@@ -92,11 +94,11 @@ set $(exiv2 -g Exif.Image.DateTime -Pv "$1")
 		HOUR="$4"
 		MINUTE="$5"
 		SECOND="$6"
-		MAKE=$(exiv2 -g Exif.Image.Make -Pv "$f")
-		MODEL=$(exiv2 -g Exif.Image.Model -Pv "$f")
+		MAKE=$(exiv2 -g Exif.Image.Make -Pv "$1")
+		MODEL=$(exiv2 -g Exif.Image.Model -Pv "$1")
  	else
-		setDATE $1
-		setTIME $1
+		setDATE "$1"
+		setTIME "$1"
 		setYEAR
 		setMONTH
 		setDAY
@@ -108,7 +110,31 @@ set $(exiv2 -g Exif.Image.DateTime -Pv "$1")
 	fi
 }
 logDirectory() {
-echo -e "\tDirectory : "$1"" >> "$LOG"
+echo -e "\tDirectory : ""$1" >> "$LOG"
 }
 logFile() {
-echo "File : "$1" "$DATE" "$TIME" "$MAKE" "$MODEL"" >> "$LOG"
+echo "File : ""$1"" ""$DATE"" ""$TIME"" ""$MAKE"" ""$MODEL" >> "$LOG"
+}
+logCopy() {
+if [[ "$FORCE" == "0" ]]; then
+	echo " copied to ""$1" >> "$LOG"
+else
+	echo " moved to ""$1" >> "$LOG"
+fi
+}
+copyFile() {
+TARGET="$TEMP/$YEAR"
+if [ ! -d "$TARGET" ]; then
+	mkdir "$TARGET"
+fi
+TARGET="$TARGET/$MONTH"
+if [ ! -d "$TARGET" ]; then
+	mkdir "$TARGET"
+fi
+TARGET="$TARGET/$DAY"
+if [ ! -d "$TARGET" ]; then
+	mkdir "$TARGET"
+fi
+cp "$1" "$TARGET"/"$YEAR"-"$MONTH"-"$DAY"_"$HOUR"-"$MINUTE"-"$SECOND"_"$MAKE"_"$MODEL"
+logCopy "$TARGET"/"$YEAR"-"$MONTH"-"$DAY"_"$HOUR"-"$MINUTE"-"$SECOND"_"$MAKE"_"$MODEL"
+}
